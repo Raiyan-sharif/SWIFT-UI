210051f9f31b929db755c4b414538f1434a5ab77
Raiyan <raiyan.sharif.1234@gmail.com>
2021-07-09 21:49:39 +0600

    Test Run

---
+++ SWIFTY/SWIFTY/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SWIFTY
+//
+//  Created by raiyan sharif on 9/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Raiyan")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
