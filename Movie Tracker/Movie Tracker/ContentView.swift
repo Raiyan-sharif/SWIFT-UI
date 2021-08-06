bca9f38dca5407a74084773ab7c176241337ae7b
Raiyan <raiyan.sharif.1234@gmail.com>
2021-08-07 02:43:51 +0600

    New Project

---
+++ Movie Tracker/Movie Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie Tracker
+//
+//  Created by raiyan sharif on 7/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
