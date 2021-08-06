0db1e0150926b8cabc1c233f13ebb2fbf594d5be
Raiyan <raiyan.sharif.1234@gmail.com>
2021-08-07 04:23:50 +0600

    Control view

--- Movie Tracker/Movie Tracker/ContentView.swift
+++ Movie Tracker/Movie Tracker/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var title = ""
+    @State var rating = 3.0
+    @State var seen = false
     var body: some View {
 //        HStack (){
 //            Text("Hello").font(.largeTitle)
@@ -20,13 +23,49 @@ struct ContentView: View {
 //            Text("Tap")
 //        })
         
-        Button(action: {}, label: {
-            HStack{
-                Text("Tap Me")
-                Image(systemName: "gamecontroller")
+//        Button(action: {}, label: {
+//            HStack{
+//                Text("Tap Me")
+//                Image(systemName: "gamecontroller")
+//            }
+//        }
+//        ).padding().background(Color.blue).cornerRadius(10).foregroundColor(.white)
+        // Controls
+        
+        List {
+            Section{
+            TextField("Movie Title", text: $title)
             }
-        }
-        ).padding().background(Color.blue).cornerRadius(10).foregroundColor(.white)
+            Section{
+                Text("Rating").font(.caption).foregroundColor(.gray)
+                HStack{
+                    Spacer()
+                    Text(String(repeating: "⭐️", count: Int(rating))).foregroundColor(.yellow).font(.title)
+                    Spacer()
+                }
+                Slider(value: $rating, in: 0...5,step: 1.0)
+            }
+            Section{
+                Toggle(isOn: $seen, label: {
+                    if title == ""{
+                        Text("Has Seen this movie")
+                    }
+                    else{
+                        Text("I Have Seen \(title)")
+                    }
+                })
+            }
+            Section{
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    HStack {
+                        Spacer()
+                        Text("Save")
+                        Spacer()
+                    }
+                })
+            }
+        }.listStyle(GroupedListStyle())
+        
     }
 }
 
